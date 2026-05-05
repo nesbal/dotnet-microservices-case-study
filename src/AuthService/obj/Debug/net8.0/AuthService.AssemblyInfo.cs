10b48260486517be4e2b9aecb98f5d904bb23c13
Nesibe Balaban <nesibebalaban1@gmail.com>
2026-05-05 12:19:46 +0300

    feat: add api/v1 routing via gateway and clean route transforms

--- src/AuthService/obj/Debug/net8.0/AuthService.AssemblyInfo.cs
+++ src/AuthService/obj/Debug/net8.0/AuthService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+960de5c81770c0fec8ca5467a540e15f8af29876")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faa49461e3d5745124ad5e2c6da60922ec9bca30")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
