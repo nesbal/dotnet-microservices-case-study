824730b64274b6a569084193c685fcaac114ea34
Nesibe Balaban <nesibebalaban1@gmail.com>
2026-05-05 13:17:49 +0300

    chore: remove weatherforecast samples from all services

--- src/Gateway/Program.cs
+++ src/Gateway/Program.cs
@@ -58,8 +58,3 @@ app.MapReverseProxy().RequireRateLimiting("fixed");
 app.UseAuthentication();
 app.UseAuthorization();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
